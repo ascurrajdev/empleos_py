d8c2eba6a0bb5cc5066d3cd8aea1780c8f12cb94
Jose Ascurra <info@joseascurradev.xyz>
2021-09-08 20:40:15 -0400

    Agregado de componentes de adminlte y mejoras en el panel de control del usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
 @extends('adminlte::page')
 @section('title','Dashboard')
 @section('content')
-    <h1>Hola a todos</h1>
+<div class="row">
+    <div class="col-lg-4">
+        <x-adminlte-small-box title="424" text="Mis Publicaciones" icon="far fa-fw fa-paper-plane"
+            theme="primary" url="#" url-text="View details"/>
+    </div>
+    <div class="col-lg-4">
+        <x-adminlte-small-box title="424" text="Mis Postulaciones" icon="far fa-fw fa-paper-plane"
+            theme="purple" url="#" url-text="View details"/>
+    </div>
+    <div class="col-lg-4">
+        <x-adminlte-small-box title="424" text="Mis Postulaciones" icon="far fa-fw fa-paper-plane"
+            theme="purple" url="#" url-text="View details"/>
+    </div>
+</div>
 @endsection
 @section('js')
     <script src="{{mix('js/app.js')}}"></script>
